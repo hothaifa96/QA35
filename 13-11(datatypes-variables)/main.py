134bcd248cc703ae6f716facbab2884e59f10179
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-11-13 18:57:40 +0200

    message

---
+++ 13-11(datatypes-variables)/main.py
@@ -0,0 +1,25 @@
+# # print('hello world!')
+# # print('hothaifa zoubi')
+# # # this code was a test
+# # # this is a comment row
+# # print(14)  # int
+# # print(18.1)  # float
+# # print('banana')  # str
+#
+# # print('1+6')  # 1+6
+# # print(1+6)  # add
+# # print(1-6)  # sub
+# # print(2*3)  # multi
+#
+#
+# print(10 / 4)  # divide with floating point and remaining
+#
+# print(10 // 4)  # divide and return the whole value
+#
+# print(10 % 3)  # modulo (the remain)
+#
+# print(2**4)  # power
+#
+
+
+
---
+++ 13-11(datatypes-variables)/varialbes.py
@@ -0,0 +1,66 @@
+# ================ Variables ===============
+
+# name = 'hodi'  # declaration
+# print(name)
+# name = 'david'  # override
+# print(name)
+
+# DRY - dont repeat yourself
+
+
+# x = 30
+# y = 5
+#
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x//y)
+
+# variables name rules
+
+# 3q = 1 # no
+# $hodi = 1 no
+#  hodi =1 # no
+#
+# name = 'ruth'
+# last_name = 'guedalia'
+
+# ============== datatypes =============
+
+# food = 'pizza'
+#
+# print(food)
+#
+# print(type(food))
+# print(type(11))
+# print(type(-9.5))
+
+#  type() # receives value and return the data type
+
+# number = str(3)  # '3'
+# print(type(number))
+# print(number+1) # error str + int
+
+# number = int(3)  # 3
+# print(type(number))
+# print(number+1)
+
+# number = float(3)  # 3.0
+# print(type(number))
+# print(number + 1.3)  # 4.3
+
+# age = '3'
+# age = int(age)  # casting   $$
+# print(age)
+
+# ================= multiple variables =================
+
+# x, y, name = 4.7, 6, 'hamad'
+# print(x)
+# print(y)
+# print(name)
+
+a = b = c = 'kawabanga'
+print(a)
+print(b)
+print(c)
