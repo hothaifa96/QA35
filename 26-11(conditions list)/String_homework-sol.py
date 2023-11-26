5f5d9cde5ddeec63655cdf1e3fef8591f6684995
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-11-26 18:49:21 +0200

    homewrk sol

---
+++ 26-11(conditions list)/String_homework-sol.py
@@ -0,0 +1,28 @@
+# 1
+# name = input('please enter your name :')
+# print(f'hello : {name}')
+#
+
+# 2
+# sen = input('please enter a quote : ')
+# print(sen.upper())
+# # 3
+# text = input('please enter a text : ')
+# print(f'the text length is { len(text) }')
+
+# # 4
+# sen2 = input('enter 2 words :')
+# si = sen2.index(' ')
+# word2 = sen2[si + 1:]
+# print(word2[::-1])
+#
+# ip = input('please enter ip address : ')  # ip
+# si = ip.index('/')
+# print(ip[:si])
+# print(ip[si + 1:])
+
+# 5
+
+# full_name = input('please enter your name: ')
+#
+# print(f'{full_name[-3:]}{full_name[:3]}')
