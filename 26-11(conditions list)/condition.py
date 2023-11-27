b1044be701509a227649188b53baca877f088eab
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-11-27 09:39:03 +0200

    if

---
+++ 26-11(conditions list)/condition.py
@@ -0,0 +1,94 @@
+# ========= if ===========
+
+# age = int(input('please enter your age : '))
+#
+# if age >= 30:
+#     # True block
+#     print('hello')
+# else:
+#     # False block
+#     print('come back in few years kido ')
+
+# if condition :
+#     true block
+# else:
+#     false block
+
+# # balance check
+# balance = int(input('please enter your balance : '))
+# withdraw = int(input('how much do you want to chash out '))
+#
+# if withdraw <= balance:
+#     print('money on the way')  # true block
+# else:
+#     print('you cannot withdraw this much')
+#
+# print('thankyou')  # main block
+
+
+# write a code to print the
+# greater number between 2 numbers form the user
+
+# a = int(input('please enter a number'))
+# b = int(input('please enter a number'))
+#
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# write a code to print if the number is odd or even number
+
+# number = int(input('please enter a number : '))
+#
+# if number % 2 == 0:  # check if the number odd or even
+#     print('even')
+# else:
+#     print('odd')
+
+
+# ======= calculater =========
+
+# x = int(input('please enter a number : '))
+# symbol = input('please enter a operator :')
+# y = int(input('please enter a number : '))
+#
+# if symbol == '+':
+#     print(f'{x} + {y} = {x + y}  ')
+# elif symbol == '-':
+#     print(f'{x} - {y} = {x - y}  ')
+# elif symbol == '*':
+#     print(f'{x} * {y} = {x * y}  ')
+# elif symbol == '/':
+#     print(f'{x} / {y} = {x / y}  ')
+# else:
+#     print('choose a right symbol next time')
+
+
+# ####### syntax ##########
+# if condition1 :
+#     True block1
+# elif condition2:
+#     True block2
+# else:
+#     False block
+
+#          1000 %5     6000   20%
+# rami levi  if total > 1000  5%
+# rami levi  if total > 6000  20%
+# write a python code to calc the new amount according to
+# the rami levi sale table
+
+# total = int(input('please enter your  total '))
+#
+# if total > 6000:
+#     print(f'you got 20% discount new amount {total * 0.8}')
+# elif total > 1000:
+#     print(f'you got 5% discount new amount {total * 0.95}')
+# else:
+#     print(f'spend more to earn more {total}')
+
+
+x = int(input('enter a number'))
+
+print(f'x  -> {x}')
